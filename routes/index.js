6c6ff911702eedbae34a694dbfec4ef18974fa0e
kosziwka123 <jkosziwka@outlook.com>
2020-02-07 19:21:25 -0500

    Final Push

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,28 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'About us' });
+});
+
+router.get('/chip', function(req, res, next) {
+  res.render('chip', { title: 'CHIP Learn to play program' });
+});
+
+router.get('/teams', function(req, res, next) {
+  res.render('teams', { title: 'Rep Teams' });
+});
+
+router.get('/development', function(req, res, next) {
+  res.render('development', { title: 'Development' });
+});
+
+router.get('/contact', function(req, res, next) {
+  res.render('contact', { title: 'Contact' });
+});
+
+router.get('/faq', function(req, res, next) {
+  res.render('faq', { title: 'FAQ' });
+});
+
 module.exports = router;
